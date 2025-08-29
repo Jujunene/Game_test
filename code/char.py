af3fe405eb5ad386d911ed7dfb485a31170e6252
Mikazinha <Mika@gmail.com>
2025-08-28 23:36:53 -0300

    Adding classes that i probably will use from StarUML.

---
+++ code/char.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+
+
+class Char(Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
+    def attack(self, ):
+        pass
